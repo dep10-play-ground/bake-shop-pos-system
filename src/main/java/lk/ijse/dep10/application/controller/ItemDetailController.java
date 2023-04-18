1d78c8d20a4665370eb68f5fb92e10225859a6c7
Rashmitha Piyushan <rashmithapiyushansl@gmail.com>
2023-04-19 02:23:45 +0530

    develop item detail code

--- src/main/java/lk/ijse/dep10/application/controller/ItemDetailController.java
+++ src/main/java/lk/ijse/dep10/application/controller/ItemDetailController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep10.application.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,13 +8,17 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.dep10.application.db.DBConnection;
 import lk.ijse.dep10.application.model.ItemDetail;
+
+import java.math.BigDecimal;
+import java.sql.*;
 
 public class ItemDetailController {
 
     public Button btnAddNewItem;
     public TextField txtSearch;
-    public ComboBox cmbCompanyName;
+    public ComboBox <String> cmbCompanyName;
     @FXML
     private Button btnRemoveItem;
 
@@ -35,13 +40,71 @@ public class ItemDetailController {
     private TextField txtUnitPrice;
 
     public void initialize(){
+        loadData();
         tblItemDetails.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("itemCode"));
         tblItemDetails.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("itemName"));
-        tblItemDetails.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("companyCode"));
-        tblItemDetails.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("companyName"));
-        tblItemDetails.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        tblItemDetails.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("companyName"));
+        tblItemDetails.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        loadCompanyName();
+        tblItemDetails.getSelectionModel().selectedItemProperty().addListener((value,ol,current)->{
+            if(current==null)return;
+            txtItemName.setText(current.getItemName());
+            txtItemCode.setText(current.getItemCode());
+            txtUnitPrice.setText(current.getUnitPrice()+"");
+            txtCompanyCode.setText(current.getCompanyCode());
+            cmbCompanyName.setValue(current.getCompanyName());
+        });
 
+        cmbCompanyName.getSelectionModel().selectedItemProperty().addListener((value,old,current)->{
+            if(current==null)return;
+            Connection connection = DBConnection.getInstance().getConnection();
+            try {
+                PreparedStatement stm = connection.prepareStatement("SELECT company_id FROM Company_Details WHERE company_name=?");
+                stm.setString(1,current);
+                ResultSet rst = stm.executeQuery();
+                rst.next();
+                String companyCode = rst.getString("company_id");
+                txtCompanyCode.setText(companyCode);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    private void loadData(){
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql="Select * FROM Item";
+        try {
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery(sql);
+            PreparedStatement stm2 = connection.prepareStatement("SELECT company_name FROM Company_Details WHERE company_id=?");
+            while(rst.next()){
+                String itemCode=rst.getString("item_code");
+                String itemName=rst.getString("item_name");
+                BigDecimal unitPrice=rst.getBigDecimal("unit_price");
+                String companyId = rst.getString("company_id");
+                stm2.setString(1,companyId);
+                ResultSet rst2 = stm2.executeQuery();
+                rst2.next();
+                String companyName=rst2.getString("company_name");
+                ItemDetail loadItem=new ItemDetail(itemCode,itemName,companyId,companyName,unitPrice);
+                tblItemDetails.getItems().add(loadItem);
+            }
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadCompanyName(){
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT company_name FROM Company_Details");
+            while (rst.next()){
+                cmbCompanyName.getItems().add(rst.getString("company_name"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -51,14 +114,39 @@ public class ItemDetailController {
 
     @FXML
     void btnSaveOnAction(ActionEvent event) {
+        if(!isDataValid())return;
 
+    }
+    private boolean isDataValid(){
+        boolean dataValid=true;
+        if(txtUnitPrice.getText().isEmpty() || !(txtUnitPrice.getText().matches("\\d{1,}"))){
+            txtUnitPrice.requestFocus();
+            dataValid=false;
+        }
+        if(txtCompanyCode.getText().isEmpty() || cmbCompanyName.getValue().isEmpty()){
+            txtCompanyCode.requestFocus();
+            dataValid=false;
+        }
+        if(txtItemName.getText().isEmpty() || !(txtItemName.getText().matches("[A-Za-z]+"))){
+            txtItemName.requestFocus();
+            dataValid=false;
+        }
+        return dataValid;
     }
 
     public void btnAddNewItemOnAction(ActionEvent actionEvent) {
-//        txtItemCode.setText(itemCodeGenerate());
-//        txtItemName.clear();
-//        txtCompanyCode.clear();
-//        txtUnitPrice.clear();
+        txtItemCode.setText(itemCodeGenerate());
+        txtItemName.clear();
+        txtCompanyCode.clear();
+        txtUnitPrice.clear();
+        tblItemDetails.getSelectionModel().clearSelection();
+
     }
+    private String itemCodeGenerate(){
+       return tblItemDetails.getItems().isEmpty()?"I001":String.format("I%03d",Integer.parseInt(tblItemDetails.getItems().
+                get(tblItemDetails.getItems().size()-1).getItemCode().substring(1))+1);
+
+    }
+
 }
 
