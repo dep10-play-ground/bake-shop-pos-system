3568a3dbcd3658560f7c797440c85c54aba37ac7
Bhathiya Wimalasinghe <bhathiyawimalasinghe@gmail.com>
2023-04-18 11:53:55 +0530

    Modifiy logout

--- src/main/java/lk/ijse/dep10/application/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep10/application/controller/MainSceneController.java
@@ -3,11 +3,13 @@ package lk.ijse.dep10.application.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -37,8 +39,12 @@ public class MainSceneController {
     private Pane subScene;
 
     @FXML
-    void btnLogoutOnAction(ActionEvent event) {
-
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        Scene scene = new Scene(new FXMLLoader(getClass().getResource("/view/LoginView.fxml")).load());
+        Stage stage = (Stage) btnLogout.getScene().getWindow();
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.centerOnScreen();
     }
 
     @FXML
