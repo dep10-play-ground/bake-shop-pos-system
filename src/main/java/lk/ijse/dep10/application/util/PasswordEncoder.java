2ba53e3c8ba6ec3a90ba8a6143cf85f133efe4e2
swarna bandara <swarnalevis@gmail.com>
2023-04-23 11:22:25 +0530

    Password encorder added

---
+++ src/main/java/lk/ijse/dep10/application/util/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.application.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class PasswordEncoder {
+    public static String encode(String rawPassword){
+        return DigestUtils.sha256Hex(rawPassword);
+    }
+
+    public static boolean matches(String rawPassword, String encodedPassword){
+        return encode(rawPassword).equals(encodedPassword);
+    }
+}
