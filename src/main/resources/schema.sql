0f76a6593a214a77a940adf363cf882992f3097e
swarna bandara <swarnalevis@gmail.com>
2023-04-27 16:00:45 +0530

    schema updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -46,6 +46,16 @@ CREATE TABLE IF NOT EXISTS company_order
     CONSTRAINT cmp_order_fk_un FOREIGN KEY (username) REFERENCES Employee (user_name)
 );
 
+CREATE TABLE IF NOT EXISTS item_details
+(
+    item_code  VARCHAR(15) NOT NULL,
+    item_name  VARCHAR(50) NOT NULL,
+    company_id VARCHAR(15) NOT NULL,
+    CONSTRAINT pk_item_details PRIMARY KEY (item_code),
+    CONSTRAINT fk_item_details FOREIGN KEY (company_id) REFERENCES company (company_id)
+);
+
+/*Item store Related*/
 CREATE TABLE IF NOT EXISTS supplied_item
 (
     bill_id    VARCHAR(10)   NOT NULL,
@@ -57,7 +67,6 @@ CREATE TABLE IF NOT EXISTS supplied_item
     CONSTRAINT fk_item_code FOREIGN KEY (item_code) REFERENCES item_details (item_code)
 );
 
-/*Item store Related*/
 CREATE TABLE IF NOT EXISTS item_batch
 (
     item_code  VARCHAR(15)   NOT NULL,
@@ -66,15 +75,6 @@ CREATE TABLE IF NOT EXISTS item_batch
     unit_price DECIMAL(8, 2) NOT NULL,
     CONSTRAINT pk_item_batch PRIMARY KEY (item_code, batch_no),
     CONSTRAINT fk_item_batch FOREIGN KEY (item_code) REFERENCES item_details (item_code)
-);
-
-CREATE TABLE IF NOT EXISTS item_details
-(
-    item_code  VARCHAR(15) NOT NULL,
-    item_name  VARCHAR(50) NOT NULL,
-    company_id VARCHAR(15) NOT NULL,
-    CONSTRAINT pk_item_details PRIMARY KEY (item_code),
-    CONSTRAINT fk_item_details FOREIGN KEY (company_id) REFERENCES company (company_id)
 );
 
 /*Production item Related*/
@@ -95,8 +95,5 @@ CREATE TABLE IF NOT EXISTS production_item_details
     quantity  INT         NOT NULL,
     CONSTRAINT pk_item_details PRIMARY KEY (order_id, batch_no, item_code),
     CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES production_item (order_id),
-    CONSTRAINT fk_batch_no FOREIGN KEY (batch_no) REFERENCES item_batch (batch_no),
-    CONSTRAINT fk_item_code FOREIGN KEY (item_code) REFERENCES item_details (item_code)
+    CONSTRAINT fk_batch_no FOREIGN KEY (item_code, batch_no) REFERENCES item_batch(item_code, batch_no)
 );
-
-
